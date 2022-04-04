dd70d441695cca31cc9ccf549eca431e567822f1
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-04-05 06:39:33 +0700

    chore: change props interface

--- src/components/atoms/caption/Caption.tsx
+++ src/components/atoms/caption/Caption.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {StyleSheet, Text} from 'react-native';
 
 interface CaptionProps {
-  children: string;
+  children: ReactNode;
   fontSize: number;
 }
 
