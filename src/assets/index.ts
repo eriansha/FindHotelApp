aadc0480569697629253e67b8b1dd61ff6133597
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-04-07 06:27:38 +0700

    chore: import image asset in index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,9 @@
 const mainHotel1 = require('./img/mains/hotel_1.jpeg');
 const mainHotel2 = require('./img/mains/hotel_2.jpeg');
+const hotelItem1 = require('./img/items/hotel_item_1.jpeg');
+const hotelItem2 = require('./img/items/hote_item_2.jpeg');
+const hotelItem3 = require('./img/items/hotel_item_3.jpeg');
+const hotelItem4 = require('./img/items/hotel_item_4.jpeg');
 
 const pinIcon = require('./icon/pin.svg');
 const starIcon = require('./icon/star.svg');
@@ -8,6 +12,10 @@ export {
   // image
   mainHotel1,
   mainHotel2,
+  hotelItem1,
+  hotelItem2,
+  hotelItem3,
+  hotelItem4,
   // icon
   pinIcon,
   starIcon,
