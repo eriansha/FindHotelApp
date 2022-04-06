6e976e924c3d4b99e701dba7739a9e405aec843c
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-04-07 06:01:30 +0700

    style: generate native icon for HotelCard

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+const mainHotel1 = require('./img/mains/hotel_1.jpeg');
+const mainHotel2 = require('./img/mains/hotel_2.jpeg');
+
+const pinIcon = require('./icon/pin.svg');
+const starIcon = require('./icon/star.svg');
+
+export {
+  // image
+  mainHotel1,
+  mainHotel2,
+  // icon
+  pinIcon,
+  starIcon,
+};
