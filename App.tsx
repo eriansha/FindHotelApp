205cc0c23bb80c2ef41bb5d303a47e0953807fd9
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-04-14 05:55:37 +0700

    chore: install react-navigation and its dependencies

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
 import HomeScreen from './src/components/pages/home-screen';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {View, Text} from 'react-native';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Test" component={TestScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const TestScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Test Screen</Text>
+    </View>
+  );
 };
 
 export default App;
--- android/app/src/main/java/com/pixelhotelapp/MainActivity.java
+++ android/app/src/main/java/com/pixelhotelapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pixelhotelapp;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "PixelHotelApp";
+  }
+
+  /**
+   * Required as installation react-navigation
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
