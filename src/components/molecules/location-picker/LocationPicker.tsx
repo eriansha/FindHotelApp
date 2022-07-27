c7c3c4d7414a40d17a19da75cf6f29ef4326ad49
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-07-27 21:05:15 +0700

    feature: initialize location-picker
    - use dummy data
    - use simple design

---
+++ src/components/molecules/location-picker/LocationPicker.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+export interface Location {
+  country: string;
+  city: string;
+}
+
+interface LocationPickerProps {
+  location: Location;
+  onPress: () => void;
+}
+
+const LocationPicker: React.FC<LocationPickerProps> = ({location, onPress}) => {
+  const {country, city} = location;
+
+  return (
+    <>
+      <Text style={styles.label}>Your Locations</Text>
+      <TouchableOpacity onPress={onPress}>
+        <Text style={styles.location}>
+          <Text style={styles.city}>{city}</Text>
+          {', '}
+          {country}
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: 'Poppins',
+    letterSpacing: 0.5,
+    color: '#B7B7B7',
+    fontSize: 12,
+    marginBottom: 4,
+  },
+  location: {
+    letterSpacing: 0.5,
+    fontFamily: 'Poppins',
+    fontSize: 16,
+  },
+  city: {
+    fontWeight: '700',
+  },
+});
+
+export default LocationPicker;
--- src/components/pages/search-screen/SearchScreen.tsx
+++ src/components/pages/search-screen/SearchScreen.tsx
@@ -1,12 +1,28 @@
+import LocationPicker from '../../molecules/location-picker';
+import {Location} from '../../molecules/location-picker/LocationPicker';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const DUMMY_LOCATION: Location = {
+  city: 'Bandung',
+  country: 'Indonesia',
+};
 
 const SearchScreen: React.FC = () => {
+  const handlePressLocation = () => {};
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Search Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <LocationPicker location={DUMMY_LOCATION} onPress={handlePressLocation} />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default SearchScreen;
