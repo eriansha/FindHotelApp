1de8d2e40b9b0f531f92d9c695092024033f9dd2
Ivan Putra Eriansya <eriansha.van@gmail.com>
2022-08-03 21:13:59 +0700

    feature: add SearhBar

---
+++ src/components/atoms/search-bar/SearchBar.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, TextInput, StyleSheet, ViewStyle} from 'react-native';
+import VectorImage from 'react-native-vector-image';
+import {inactiveSearchIcon} from '../../../assets';
+
+const PLACEHOLDER_INPUT = 'Find your Favorite Hotels';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    borderColor: '#F1F1F1',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 50,
+    flexDirection: 'row',
+  },
+  searhIcon: {
+    marginLeft: 24,
+    marginRight: 12,
+    alignSelf: 'center',
+  },
+  textInput: {
+    fontFamily: 'Poppins',
+    color: '#F1F1F1',
+  },
+});
+
+interface SearchBarProps {
+  style?: ViewStyle;
+  onChangeSearchText: (text: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({style, onChangeSearchText}) => {
+  return (
+    <View style={[styles.container, style]}>
+      <VectorImage style={styles.searhIcon} source={inactiveSearchIcon} />
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={onChangeSearchText}
+        placeholder={PLACEHOLDER_INPUT}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
--- src/components/pages/search-screen/SearchScreen.tsx
+++ src/components/pages/search-screen/SearchScreen.tsx
@@ -1,7 +1,8 @@
 import LocationPicker from '../../molecules/location-picker';
 import {Location} from '../../molecules/location-picker/LocationPicker';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import SearchBar from '../../atoms/search-bar/SearchBar';
 
 const DUMMY_LOCATION: Location = {
   city: 'Bandung',
@@ -9,11 +10,23 @@ const DUMMY_LOCATION: Location = {
 };
 
 const SearchScreen: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [searchText, setSearchText] = useState<string>();
+
   const handlePressLocation = () => {};
+
+  const handleChangeSearchText = (text: string) => {
+    setSearchText(text);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <LocationPicker location={DUMMY_LOCATION} onPress={handlePressLocation} />
+
+      <SearchBar
+        style={styles.searhBar}
+        onChangeSearchText={handleChangeSearchText}
+      />
     </SafeAreaView>
   );
 };
@@ -22,6 +35,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
+    backgroundColor: '#FBFBFB',
+  },
+  searhBar: {
+    marginVertical: 12,
   },
 });
 
